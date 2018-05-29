74f32eb0957ee01e420ff133ac45bef0e464868e
Yang Kyowon <kyowonhorang@gmail.com>
2018-05-29 16:50:36 +0900

    Impl Game play method

--- Sources/GameInstance.cpp
+++ Sources/GameInstance.cpp
@@ -7,7 +7,8 @@ namespace BattleShip
       m_mode( mode ),
       m_width( width ), m_height( height ),
       m_turnCount( 0 ),
-      m_attacker( nullptr ), m_defender( nullptr )
+      m_attacker( nullptr ), m_defender( nullptr ),
+      m_bGameEnd( false )
    {
       m_attacker = new Player( width, height );
       m_defender = new Player( width, height );
--- Sources/GameInstance.h
+++ Sources/GameInstance.h
@@ -11,24 +11,40 @@ namespace BattleShip
       GameInstance( int width, int height, GameMode mode );
       virtual ~GameInstance( );
 
+      virtual void Play( ) final
+      {
+         this->Init( );
+         m_bGameEnd = false;
+         while ( !m_bGameEnd )
+         {
+            this->Update( );
+            this->Render( );
+         }
+      }
+
       virtual void Init( ) = 0;
       virtual void Update( ) = 0;
       virtual void Render( ) = 0;
 
       GameMode GetMode( ) const { return m_mode; }
+      inline void IncreaseTurnCount( ) { ++m_turnCount; }
       int GetTurnCount( ) const { return m_turnCount; }
 
       Player& GetAttacker( ) { return ( *m_attacker ); }
       Player& GetDefender( ) { return ( *m_defender ); }
 
    protected:
+      Player*  m_attacker;
+      Player*  m_defender;
+
+      bool     m_bGameEnd;
+
+   private:
       GameMode m_mode;
       int m_width;
       int m_height;
       int m_turnCount;
 
-      Player*  m_attacker;
-      Player*  m_defender;
 
    };
 }
