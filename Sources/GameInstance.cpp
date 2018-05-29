abe4458f07b703cded7000742d9e7753b8b019e7
Yang Kyowon <kyowonhorang@gmail.com>
2018-05-29 17:11:32 +0900

    Added hitmap at gameinst

--- Sources/GameInstance.cpp
+++ Sources/GameInstance.cpp
@@ -8,15 +8,43 @@ namespace BattleShip
       m_width( width ), m_height( height ),
       m_turnCount( 0 ),
       m_attacker( nullptr ), m_defender( nullptr ),
+      m_hitMap( nullptr ),
       m_bGameEnd( false )
    {
       m_attacker = new Player( width, height );
       m_defender = new Player( width, height );
+
+      m_hitMap = new HitResultType*[ height ];
+      for ( int y = 0; y < m_height; ++y )
+      {
+         m_hitMap[ y ] = new HitResultType[ m_width ];
+
+         // Initilaize
+         for ( int x = 0; x < m_width; ++x )
+         {
+            m_hitMap[ y ][ x ] = HitResultType::NONE;
+         }
+      }
    }
 
    GameInstance::~GameInstance( )
    {
-      SAFE_DELETE( m_attacker );
+      if ( m_hitMap != nullptr )
+      {
+         for ( int y = 0; y < m_height; ++y )
+         {
+            if ( m_hitMap[ y ] != nullptr )
+            {
+               delete[ ] m_hitMap[ y ];
+               m_hitMap[ y ] = nullptr;
+            }
+         }
+
+         delete[ ] m_hitMap;
+         m_hitMap = nullptr;
+      }
+
       SAFE_DELETE( m_defender );
+      SAFE_DELETE( m_attacker );
    }
 }
--- Sources/GameInstance.h
+++ Sources/GameInstance.h
@@ -22,10 +22,6 @@ namespace BattleShip
          }
       }
 
-      virtual void Init( ) = 0;
-      virtual void Update( ) = 0;
-      virtual void Render( ) = 0;
-
       GameMode GetMode( ) const { return m_mode; }
       inline void IncreaseTurnCount( ) { ++m_turnCount; }
       int GetTurnCount( ) const { return m_turnCount; }
@@ -34,10 +30,17 @@ namespace BattleShip
       Player& GetDefender( ) { return ( *m_defender ); }
 
    protected:
+      virtual void Init( ) = 0;
+      virtual void Update( ) = 0;
+      virtual void Render( ) = 0;
+
+   protected:
       Player*  m_attacker;
       Player*  m_defender;
 
       bool     m_bGameEnd;
+
+      HitResultType**  m_hitMap;
 
    private:
       GameMode m_mode;
