a4cfc2fa60ee635c305bca2f05bc1f8d32de34f8
Yang Kyowon <kyowonhorang@gmail.com>
2018-05-30 17:09:34 +0900

    modify constant

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,11 +1,15 @@
 #include "App.h"
 
 int main( )
-{
+{  
+   constexpr int WindowWidth = 48;
+   constexpr int WindowHeight = 27;
+   constexpr int BoardWidth = 10;
+   constexpr int BoardHeight = 10;
    BattleShip::App app{ 
       "бс < BattleShip > бс",
-      55, 27,
-      10, 10,
+      WindowWidth, WindowHeight,
+      BoardWidth, BoardHeight,
       BattleShip::GameMode::SUPERVISED };
 
    return app.Run( );
